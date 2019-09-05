ee8b3a6f568a324a089c66d15e54befc0879872f
Laurence <admin@halfslice.studio>
2019-09-06 00:07:49 +1000

    First
    Created the main UI scene and a basic UIVars script that holds data for UI

---
+++ Cycles-Unity3D/Assets/Scripts/UI/UIVars.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+//Sadly VS has issues seeing this package. But it does still compile in unity!
+using UnityEngine.UI;
+public class UIVars : MonoBehaviour
+{
+    public Text[] TextObjs;
+    public InputField[] FieldObjs;
+    public Dropdown[] ComboObjs;
+    public Button[] ButtonObjs;
+    public Image[] ImageObjs;
+    public GameObject[] Objs;
+}
